9bbe53f66171de14c4284ddf9513681ea8d8079d
vandeurzenbob@gmail.com <vandeurzenbob@gmail.com>
2024-12-17 15:17:20 +0000

    Fixing operational margin in intermedia model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,3 +1,6 @@
-SELECT *,
-ROUND((margin+shipping_fee)-logcost-ship_cost) as operational_margin
-FROM {{ ref("int_orders_margin") }}
+SELECT orders_id,date_date,
+ROUND((margin+shipping_fee-logcost-ship_cost),2) as operational_margin,
+quantity,purchase_cost
+FROM {{ ref("int_orders_margin") }} AS int_margin
+JOIN {{ ref("stg_raw__ship") }} AS raw_ship
+USING (orders_id)
