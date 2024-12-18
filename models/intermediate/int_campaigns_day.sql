e8ce72591a8204967a256c3faf191964521df2e6
Bob Vandeurzen <vandeurzenbob@gmail.com>
2024-12-18 14:39:34 +0000

    intermediate models updated

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -2,6 +2,7 @@ select
     date_date,
     sum(ads_cost) as ads_cost,
     sum(impression) as ads_impressions,
-    sum(click) as ads_clicks
-
-from {{ ref("int__campaigns") }}
+    sum(click) as ads_clicks,
+from {{ ref("int_campaigns") }}
+group by date_date
+order by date_date desc
